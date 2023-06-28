044b802a6024d1b2fa04b2c0127526087e3f78d9
ffekirnew <ffekirnew0808@gmail.com>
2023-06-28 05:44:46 +0300

    Added swagger documentation and JSDoc annotations for the routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const db = require('./config/db_connect');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 const router = require('./routes/authRoutes');
 const foodRouter = require('./routes/foodRoutes');
 const orderRouter = require('./routes/orderRoutes');
@@ -9,6 +13,21 @@ const paymentRouter = require('./routes/paymentRoutes');
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+// Swagger configuration
+const swaggerSpec = swaggerJsdoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Gorana Food Delivery Web App API',
+      version: '1.0.0',
+      description: 'Gorana Food Delivery Web App API documentation',
+    },
+  },
+  apis: ['./routes/*'], // Specify the file(s) where your JSDoc annotations are present
+});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
@@ -22,6 +41,7 @@ app.use(router);
 app.use(foodRouter);
 app.use(orderRouter);
 app.use(paymentRouter);
+
 app.listen(port,()=>{
     console.log(`listening on ${port}`)
 })
